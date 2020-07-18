b3d4791877955105539db315fc7d5204beccc696
Ashley Lake <ashley.p.lake@gmail.com>
2020-07-18 23:35:09 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 and returns the phrase
 
-def parrot (phrase = "Squaks!")
+def parrot(phrase = "Squaks!")
   puts phrase
   returns phrase
 end
